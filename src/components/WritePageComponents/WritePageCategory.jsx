0d72cd7bea6e632d9882b47d12ba9611b56c730b
gilhun <qkrrlfgns123@gmail.com>
2023-11-24 16:50:02 +0900

    WritePage 데이터베이스 연동

--- src/components/WritePageComponents/WritePageCategory.jsx
+++ src/components/WritePageComponents/WritePageCategory.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
-import * as S from '../MainPageComponents/MainPageComponents.styled'
+
 import { WPCategoryDiv } from './WritePageComponents.styled'
 
-const WritePageCategory = () => {
+const WritePageCategory = ({ setCategory, category, image, setImage }) => {
+  const changeCategoryHandler = (e) => {
+    setCategory(e.target.value)
+  }
+  const changeImageHandler = (e) => {
+    setImage(e.target.files)
+  }
+
   return (
     <WPCategoryDiv>
       <label htmlFor="uploadFile"></label>
-      <input id="uploadFile" type="file" />
-      <select>
-        <option>카테고리</option>
+      <input
+        id="uploadFile"
+        type="file"
+        onChange={changeImageHandler}
+        files={image}
+      />
+      <select onChange={changeCategoryHandler} value={category}>
+        <option value="lol">리그오브레전드</option>
+        <option value="Maple">메이플스토리</option>
+        <option value="val">발로란트</option>
       </select>
     </WPCategoryDiv>
   )
--- src/components/WritePageComponents/WritePageSubmit.jsx
+++ src/components/WritePageComponents/WritePageSubmit.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import {
   WPcontainerForm,
@@ -7,19 +7,22 @@ import {
   CompleteBtn,
 } from './WritePageComponents.styled'
 import WritePageCategory from './WritePageCategory'
-import { MyProfileUploadImg } from '../../styles/images'
+import { useSelector } from 'react-redux'
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
+import { db, pathReference } from '../../API/Firebase/Firebase'
+import { doc, setDoc } from 'firebase/firestore'
 
 const WritePageSubmit = () => {
   // 상태 변수들
-  const [category, setCategory] = useState('')
+  const [category, setCategory] = useState('lol')
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
-  const [cards, setCards] = useState([])
-
-  // 카테고리 선택 핸들러
-  const categoryChangeHandler = (selectedCategory) => {
-    setCategory(selectedCategory)
-  }
+  // const [cards, setCards] = useState([])
+  const [image, setImage] = useState('')
+  const currentUser = useSelector((state) => state.user.currentUserInfo)
+  useEffect(() => {
+    console.log(image)
+  }, [image])
 
   // 제목 입력 핸들러
   const titleChangeHandler = (e) => {
@@ -34,32 +37,44 @@ const WritePageSubmit = () => {
   const onSubmitHandler = (e) => {
     e.preventDefault()
     if (!title || !content) {
-      window.alert('제목과 내용 그리고 카테고리를 모두 입력해주세요.')
+      window.alert('제목과 내용을 모두 입력해주세요.')
       return
     }
 
-    // 새로운 카드 객체 생성
-    const newCard = {
-      id: uuidv4(),
-      title,
-      content,
-      writefor: category,
-      createdAt: new Date(),
+    const userImageRouteRef = ref(pathReference, `userImage/${image[0].name}`)
+
+    const uploadNewPostHandler = async () => {
+      await uploadBytes(userImageRouteRef, image[0])
+      const url = await getDownloadURL(userImageRouteRef)
+
+      const postId = uuidv4()
+      const newPost = {
+        category,
+        content,
+        id: postId,
+        imgsrc: url,
+        title,
+        user: currentUser.displayName,
+        userProfile: currentUser.photoURL,
+        uid: currentUser.uid,
+        date: new Date(),
+      }
+      await setDoc(doc(db, 'post', postId), newPost)
     }
-
-    // 기존 카드 목록에 새로운 카드 추가
-    setCards((prevCards) => [...prevCards, newCard])
-
+    uploadNewPostHandler()
     // 입력 폼 초기화
     setTitle('')
     setContent('')
-    alert('작성이 완료되었습니다.')
-    console.log('저장된 카드 목록:', cards)
   }
 
   return (
     <WPcontainerForm onSubmit={onSubmitHandler}>
-      <WritePageCategory />
+      <WritePageCategory
+        setCategory={setCategory}
+        category={category}
+        setImage={setImage}
+        image={image}
+      />
 
       <WPtitle
         placeholder="제목"
